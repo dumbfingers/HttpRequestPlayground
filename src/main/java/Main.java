103f015a7551dc8c1e9be07841aa0db2709abcf1
Yaxi Ye <ss1271@gmail.com>
2017-10-30 19:14:32 +0000

    Error and url handling, moving Params out to a new class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,24 @@
 import com.google.gson.JsonObject;
 import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.Headers;
 import okhttp3.ResponseBody;
 import retrofit2.Response;
 
+
 import java.util.Scanner;
 
 public class Main {
 
-    public static final String PARAM_CONTENT_LENGTH = "Content_length";
-    public static final String PARAM_URL = "Url";
-    public static final String PARAM_STATUS_CODE = "Status_code";
-    public static final String PARAM_DATE = "Date";
-    public static final String PREFIX_HTTP = "http://";
-    public static final String PREFIX_HTTPS = "https://";
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        printStartline();
+        printEntryline();
         String input = scanner.nextLine();
 
         while (!input.equals("quit")) {
             if (isURLLegal(input)) {
-                dispatchHttpRequest(input);
+                dispatchHttpRequest(addTrailingSlash(input));
             }
             input = scanner.nextLine();
         }
@@ -35,13 +30,14 @@ public class Main {
             return false;
         }
 
-        String lowercase = url.toLowerCase();
-        if (lowercase.startsWith(PREFIX_HTTP)
-                || lowercase.toLowerCase().startsWith(PREFIX_HTTPS)) {
+        String urlLowercase = url.toLowerCase();
+        if (urlLowercase.startsWith(Params.PREFIX_HTTP)
+                || urlLowercase.toLowerCase().startsWith(Params.PREFIX_HTTPS)) {
             return true;
         } else {
             System.out.println("Invalid input.\nThe website must start with \"http://\" or \"https://\"");
-            printStartline();
+            printError(url, "invalid url");
+            printEntryline();
         }
 
         return false;
@@ -54,26 +50,38 @@ public class Main {
                 .subscribeOn(Schedulers.io())
                 .subscribe(
                         response -> buildResponse(url, response),
-                        throwable -> System.out.println("Error " + throwable.toString()),
-                        Main::printStartline);
+                        throwable -> printError(url, throwable.toString()),
+                        Main::printEntryline);
     }
 
     private static void buildResponse(String url, Response<ResponseBody> response) {
-        Headers headers = response.headers();
-        String date = headers.get(PARAM_DATE);
-        int statusCode = response.code();
-        long contentLength = response.body() == null ? -1 : response.body().contentLength();
         JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty(PARAM_URL, url);
-        jsonObject.addProperty(PARAM_STATUS_CODE, statusCode);
-        if (contentLength >= 0) {
-            jsonObject.addProperty(PARAM_CONTENT_LENGTH, contentLength);
+        jsonObject.addProperty(Params.PARAM_URL, url);
+        jsonObject.addProperty(Params.PARAM_STATUS_CODE, response.code());
+        if (response.body() != null
+                && response.body().contentLength() >= 0) {
+            jsonObject.addProperty(Params.PARAM_CONTENT_LENGTH, response.body().contentLength());
         }
-        jsonObject.addProperty(PARAM_DATE, date);
+        Headers headers = response.headers();
+        jsonObject.addProperty(Params.PARAM_DATE, headers.get(Params.PARAM_DATE));
         System.out.println(jsonObject);
     }
 
-    private static void printStartline() {
+    private static void printEntryline() {
         System.out.println("Enter a website URL, then press \"Enter\" key to execute:\n(Enter quit to exit)");
+    }
+
+    private static void printError(String url, @NonNull String error) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty(Params.PARAM_URL, url);
+        jsonObject.addProperty(Params.PARAM_ERROR, error);
+        System.out.println(jsonObject);
+    }
+
+    private static String addTrailingSlash(@NonNull String url) {
+        if (!url.endsWith("/")) {
+            url = url + "/";
+        }
+        return url;
     }
 }
---
+++ src/main/java/Params.java
@@ -0,0 +1,9 @@
+public class Params {
+    public static final String PARAM_CONTENT_LENGTH = "Content_length";
+    public static final String PARAM_URL = "Url";
+    public static final String PARAM_ERROR = "Error";
+    public static final String PARAM_STATUS_CODE = "Status_code";
+    public static final String PARAM_DATE = "Date";
+    public static final String PREFIX_HTTP = "http://";
+    public static final String PREFIX_HTTPS = "https://";
+}
